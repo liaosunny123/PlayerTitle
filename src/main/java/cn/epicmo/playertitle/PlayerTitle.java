cab8c67aa7de651602d0cedd1151e9bfb2803480
epicmo <1982742309@qq.com>
2022-09-23 20:05:54 +0800

    init

---
+++ src/main/java/cn/epicmo/playertitle/PlayerTitle.java
@@ -0,0 +1,18 @@
+package cn.epicmo.playertitle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerTitle extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("s");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
